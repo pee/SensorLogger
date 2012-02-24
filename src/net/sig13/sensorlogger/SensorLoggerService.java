e3beb27369a45374bf0528701dd9e07d06b3c720
pee <pee@erkkila.org>
2012-02-24 05:50:45 +0000

    apparenty i don't want to call that ;p

--- src/net/sig13/sensorlogger/SensorLoggerService.java
+++ src/net/sig13/sensorlogger/SensorLoggerService.java
@@ -137,8 +137,7 @@ public class SensorLoggerService extends IntentService implements OnSharedPrefer
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
         mgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 60000, 60000, pi);
 
-        Notification notification = mkForegroundNotification();
-        startForeground(NOTIFICATION_ID, notification);
+
 
         // clear pending scheduled handle events for rr
         handler.removeCallbacks(rr);
@@ -153,9 +152,15 @@ public class SensorLoggerService extends IntentService implements OnSharedPrefer
     public int onStartCommand(Intent intent, int flags, int startId) {
 
         Log.d(TAG, "onStartCommand");
+        //super.onStartCommand(intent, flags, startId);
 
-        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-        return super.onStartCommand(intent, flags, startId);
+        Notification notification = mkForegroundNotification();
+        startForeground(NOTIFICATION_ID, notification);
+
+        Toast.makeText(this, "sensor logger service starting", Toast.LENGTH_SHORT).show();
+        //return super.onStartCommand(intent, flags, startId);
+
+        return mStartMode;
 
     }
 
